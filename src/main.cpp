ecc7eb60f1e14286572e8425fcbb397f588e4606
Neuxs0 <neuxsyt@gmail.com>
2025-05-16 08:59:56 -0700

    Add cube

--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,52 @@
 #include <glm/gtc/type_ptr.hpp>
 
 std::vector<Vertex> vertices = {
-    // position                     // color                        // texcoord
-    {glm::vec3(-0.5f, 0.5f, 0.0f),  glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 0.0f)},
-    {glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 1.0f)},
-    {glm::vec3(0.5f, -0.5f, 0.0f),  glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 1.0f)},
-    {glm::vec3(0.5f, 0.5f, 0.0f),   glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 0.0f)}
+    // position                         // color                        // texcoord
+    // Front face (+Z)
+    {glm::vec3(-0.5f, 0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 0.0f)},  // Top-Left
+    {glm::vec3(-0.5f, -0.5f, 0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(0.5f, -0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 1.0f)},  // Bottom-Right
+    {glm::vec3(0.5f, 0.5f, 0.5f),       glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 0.0f)},   // Top-Right
+
+    // Back face (-Z)
+    {glm::vec3(0.5f, 0.5f, -0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 0.0f)}, // Top-Left
+    {glm::vec3(0.5f, -0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(-0.5f, -0.5f, -0.5f),    glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 1.0f)}, // Bottom-Right
+    {glm::vec3(-0.5f, 0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 0.0f)}, // Top-Right
+
+    // Left face (-X)
+    {glm::vec3(-0.5f, 0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 0.0f)}, // Top-Left
+    {glm::vec3(-0.5f, -0.5f, -0.5f),    glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(-0.5f, -0.5f, 0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 1.0f)}, // Bottom-Right
+    {glm::vec3(-0.5f, 0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 0.0f)}, // Top-Right
+
+    // Right face (+X)
+    {glm::vec3(0.5f, 0.5f, 0.5f),       glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(0.0f, 0.0f)},  // Top-Left
+    {glm::vec3(0.5f, -0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(0.5f, -0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(1.0f, 1.0f)}, // Bottom-Right
+    {glm::vec3(0.5f, 0.5f, -0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(1.0f, 0.0f)}, // Top-Right
+
+    // Top face (+Y)
+    {glm::vec3(-0.5f, 0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(0.0f, 0.0f)}, // Top-Left
+    {glm::vec3(-0.5f, 0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(0.5f, 0.5f, 0.5f),       glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(1.0f, 1.0f)}, // Bottom-Right
+    {glm::vec3(0.5f, 0.5f, -0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),     glm::vec2(1.0f, 0.0f)}, // Top-Right
+
+    // Bottom face (-Y)
+    {glm::vec3(-0.5f, -0.5f, 0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 0.0f)}, // Top-Left
+    {glm::vec3(-0.5f, -0.5f, -0.5f),    glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(0.0f, 1.0f)}, // Bottom-Left
+    {glm::vec3(0.5f, -0.5f, -0.5f),     glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 1.0f)}, // Bottom-Right
+    {glm::vec3(0.5f, -0.5f, 0.5f),      glm::vec3(1.0f, 1.0f, 1.0f),    glm::vec2(1.0f, 0.0f)}  // Top-Right
 };
 
 std::vector<GLuint> indices = {
-    0, 1, 2,    // Triangle 1
-    0, 2, 3     // Triangle 2
+    0,  1,  2,  0,  2,  3,  // Front face
+    4,  5,  6,  4,  6,  7,  // Back face
+    8,  9,  10, 8,  10, 11, // Left face
+    12, 13, 14, 12, 14, 15, // Right face
+    16, 17, 18, 16, 18, 19, // Top face
+    20, 21, 22, 20, 22, 23  // Bottom face
 };
-
 
 int main() {
     // Window Settings
